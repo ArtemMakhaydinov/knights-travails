e1ee1d491a43b197ac68b032401892dac5999b43
Artem <nimoshka@gmail.com>
2022-09-06 14:11:06 +0300

    Fix output

--- knights_travails.js
+++ knights_travails.js
@@ -8,7 +8,7 @@ class Knight {
         for (let x = 0; x <= 7; x++) {
             for (let y = 1; y <= 8; y++) {
                 const moves = this.possibleMoves(x, y);
-                board.set(`${String.fromCharCode(x + 97)}, ${y}`, moves);
+                board.set(`${String.fromCharCode(x + 97)}${y}`, moves);
             }
         }
         return board;
@@ -21,19 +21,19 @@ class Knight {
 
     possibleMoves(x, y) {
         const result = [];
-        if (this.isCoordsValid(x + 1, y + 2)) result.push(`${String.fromCharCode(x + 97 + 1)}, ${y + 2}`);
-        if (this.isCoordsValid(x + 2, y + 1)) result.push(`${String.fromCharCode(x + 97 + 2)}, ${y + 1}`);
-        if (this.isCoordsValid(x + 2, y - 1)) result.push(`${String.fromCharCode(x + 97 + 2)}, ${y - 1}`);
-        if (this.isCoordsValid(x + 1, y - 2)) result.push(`${String.fromCharCode(x + 97 + 1)}, ${y - 2}`);
-        if (this.isCoordsValid(x - 1, y - 2)) result.push(`${String.fromCharCode(x + 97 - 1)}, ${y - 2}`);
-        if (this.isCoordsValid(x - 2, y - 1)) result.push(`${String.fromCharCode(x + 97 - 2)}, ${y - 1}`);
-        if (this.isCoordsValid(x - 2, y + 1)) result.push(`${String.fromCharCode(x + 97 - 2)}, ${y + 1}`);
-        if (this.isCoordsValid(x - 1, y + 2)) result.push(`${String.fromCharCode(x + 97 - 1)}, ${y + 2}`);
+        if (this.isCoordsValid(x + 1, y + 2)) result.push(`${String.fromCharCode(x + 97 + 1)}${y + 2}`);
+        if (this.isCoordsValid(x + 2, y + 1)) result.push(`${String.fromCharCode(x + 97 + 2)}${y + 1}`);
+        if (this.isCoordsValid(x + 2, y - 1)) result.push(`${String.fromCharCode(x + 97 + 2)}${y - 1}`);
+        if (this.isCoordsValid(x + 1, y - 2)) result.push(`${String.fromCharCode(x + 97 + 1)}${y - 2}`);
+        if (this.isCoordsValid(x - 1, y - 2)) result.push(`${String.fromCharCode(x + 97 - 1)}${y - 2}`);
+        if (this.isCoordsValid(x - 2, y - 1)) result.push(`${String.fromCharCode(x + 97 - 2)}${y - 1}`);
+        if (this.isCoordsValid(x - 2, y + 1)) result.push(`${String.fromCharCode(x + 97 - 2)}${y + 1}`);
+        if (this.isCoordsValid(x - 1, y + 2)) result.push(`${String.fromCharCode(x + 97 - 1)}${y + 2}`);
         return result;
     }
 
     knightMoves(start, end, path = [end]) {
-        if (start === end) return path.reverse();
+        if (start === end) return `You made it in ${path.length - 1} moves, here is your path: ${path.join(' => ')}`;
         const visited = new Set();
         const queue = [start];
         while (queue.length > 0) {
@@ -41,7 +41,7 @@ class Knight {
             const moves = this.board.get(current);
             for (const move of moves) {
                 if (move === end) {
-                    path.push(current);
+                    path.unshift(current);
                     return this.knightMoves (start, current, path)
                 }
                 if (!visited.has(move)){
@@ -56,9 +56,8 @@ class Knight {
 }
 
 const knight = new Knight();
-console.log(knight.knightMoves('a, 1', 'b, 2'));
-console.log(knight.knightMoves('d, 1', 'a, 8'));
-console.log(knight.knightMoves('f, 1', 'a, 2'));
-console.log(knight.knightMoves('d, 1', 'd, 2'));
-console.log(knight.knightMoves('e, 2', 'e, 4'));
-
+console.log(knight.knightMoves('a1', 'b2'));
+console.log(knight.knightMoves('d1', 'a8'));
+console.log(knight.knightMoves('f1', 'a2'));
+console.log(knight.knightMoves('d1', 'd2'));
+console.log(knight.knightMoves('e2', 'e4'));
